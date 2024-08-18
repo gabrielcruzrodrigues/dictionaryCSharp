64b7653688a0ad51914751d20be13e488ce3e6ce
Gabriel Rodrigues <contatogabrielcruzrodrigues@gmail.com>
2024-08-18 20:49:55 -0300

    Add project files.

---
+++ TiposDeDados/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //declaração de variavel
+        int n1 = 1;
+
+        //declaração de constante
+        const int n2 = 2
+    }
+}
---
+++ TiposDeDadosIntegrais/Program.cs
@@ -0,0 +1,29 @@
+﻿//-128 a 127
+sbyte n3 = -1;
+
+//0 a 127
+byte n4 = 1;
+
+//-32.768 a 32.767
+short n5 = -1;
+
+//0 a 65.535
+ushort n6 = 1;
+
+//inteiro que permite negativo
+int n7 = -1;
+
+//inteiro que não permite negativo
+uint n8 = 1;
+
+//long que permite negativo
+long n9 = -1;
+
+//long que não permite negativo
+ulong n10 = 1;
+
+//inteiro com sinal de 32 bits ou 64 bits
+nint n11 = 1;
+
+//inteiro sem sinal de 32 bits ou 64 bits
+nuint n12 = 1;
